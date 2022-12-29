e583219b763bc3d2384bbf175de26c1e71f4a915
shiyasmohd <shiyasjaseena124@gmail.com>
2022-12-29 22:21:34 +0530

    video nft showimg

--- components/My Nfts /NftCard.tsx
+++ components/My Nfts /NftCard.tsx
@@ -2,18 +2,33 @@ import Image from "next/image"
 import { NftDetails } from "./MyNftsPage"
 import styles from '../../styles/MyNft.module.css'
 
+
 const NftCard = (props: NftDetails) =>{
+
+
+    const getLink = async() =>{
+        console.log(props.image.split('.').pop())
+        fetch(props.image)
+            .then(response => {
+                console.log(response.headers.get("content-type")); // Output: "image/jpeg"
+            })
+            .catch(error => {
+                console.error(error);
+            });
+
+    }
+    
     return(
-        <div className={` ${styles.nftCard} w-full rounded-2xl shadow-md border`} onClick={()=>console.log(props.image.slice(0,7))}>
+        <div className={` ${styles.nftCard} w-full rounded-2xl shadow-md border`} onClick={getLink}>
             <div className="w-full h-[150px] rounded-2xl flex justify-center items-center overflow-hidden
                             md:h-[225px]">
                 {
-                    props.format == "mp4" ?
+                    props.format == "mp4" || props.image.split('.').pop() == "mp4" ?
                     <video autoPlay loop style={{ width: '500px', height: '500px' }}>
                         <source src={props.image} />
                     </video>
                     :
-                    <Image src={props.image} width={500} height={500}  alt=""/>
+                    <img src={props.image} width={500} height={500}  alt=""/>
                 }
                 
             </div>
