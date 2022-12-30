f3b08ff8ddd7976fac113c45fd966c02185cb4c5
Shiyas Mohammed <83513144+Shiyasmohd@users.noreply.github.com>
2022-12-30 10:17:47 +0530

    Update LoginPage.tsx

--- components/Login/LoginPage.tsx
+++ components/Login/LoginPage.tsx
@@ -6,7 +6,7 @@ const LoginPage = () =>{
         <div className=" login-page w-full flex justify-center items-center h-screen p-6">
             <Card css={{ mw: "450px",padding: "$10 0" }}>
                 <Card.Header>
-                    <Text b className="w-full text-center">Huddle Hack</Text>
+                    <Text b className="w-full text-center">HODL MEET</Text>
                 </Card.Header>
                 <Card.Body css={{ py: "$10" }} >
                     <div className="w-full flex flex-col items-center gap-3">
