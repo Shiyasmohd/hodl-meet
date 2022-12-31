f3fe29dba6fd11ce9fd6e0f78bf53e009df0c35f
shiyasmohd <shiyasjaseena124@gmail.com>
2022-12-31 12:42:23 +0530

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,21 @@ import { useAccount } from 'wagmi';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import Home from '../components/Home/Home';
-import MyNftsPage from '../components/My Nfts /MyNftsPage';
-import LoginPage from '../components/Login/LoginPage';
 
 
 export default function Main() {
 
   const { isConnected } = useAccount()
   const router = useRouter()
-
-  if(isConnected){
-    return <MyNftsPage/>
-  }else{
-    return <LoginPage/>
-  }
   
+  useEffect(()=>{
+    if(!isConnected){
+      router.push('/login')
+    }
+  },[isConnected])
 
+  return (
+    <Home/>
+  )
 }
 Home
